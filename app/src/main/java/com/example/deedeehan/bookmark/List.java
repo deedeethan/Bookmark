1ed0a8847d9bf2c4b054884d761ed9ed0b735a33
DeeDee <dthan@andrew.cmu.edu>
2016-01-19 00:33:52 -0500

    Added more TODOs to update page numbers and remove books from list

--- app/src/main/java/com/example/deedeehan/bookmark/List.java
+++ app/src/main/java/com/example/deedeehan/bookmark/List.java
@@ -5,7 +5,7 @@ import android.widget.EditText;
 public class List {
     private EditText[] mBooks;
     private EditText[] mPages;
-
+// TODO have a way to update page numbers/ remove books from list
     public List () {
 
     }

95ddeb0f50b4986bc3916c3cdbb87910e07377e4
DeeDee Han <dthan@andrew.cmu.edu>
2016-03-12 21:36:45 -0500

    moved the log message above the lines to start a new activity. no difference

--- app/src/main/java/com/example/deedeehan/bookmark/NewListPage.java
+++ app/src/main/java/com/example/deedeehan/bookmark/NewListPage.java
@@ -50,9 +50,10 @@ public class NewListPage extends Activity implements ListDialogFragment.ListDial
 
     @Override
     public void onDialogPositiveClick(DialogFragment dialog, String name) {
+        Log.d("from dialog fragment - ", name);
         Intent intent = new Intent(this, SavedListsPage.class);
         startActivity(intent);
-        Log.d("from dialog fragment - ", name);
+
         // pass the string to the saved lists page and add a list with that name
     }
 

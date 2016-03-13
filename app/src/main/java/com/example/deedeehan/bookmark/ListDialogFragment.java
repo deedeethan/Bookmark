b3914b8212dff6697679e336399107c1d3f3d789
DeeDee Han <dthan@andrew.cmu.edu>
2016-03-12 22:19:33 -0500

    the new SavedListsPage activity now opens when I click ok on the dialog fragment, but nothing is logged

--- app/src/main/java/com/example/deedeehan/bookmark/ListDialogFragment.java
+++ app/src/main/java/com/example/deedeehan/bookmark/ListDialogFragment.java
@@ -73,12 +73,12 @@ public class ListDialogFragment extends DialogFragment implements TextView.OnEdi
                 .setView(inflater.inflate(R.layout.dialog_name, null))
                 .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        if (EditorInfo.IME_ACTION_DONE == id) {
+                        //if (EditorInfo.IME_ACTION_DONE == id) {
                             // Return input text back to activity through the implemented listener
                             mListener.onDialogPositiveClick(ListDialogFragment.this, mEditText.getText().toString());
                             // Close the dialog and return back to the parent activity
-                            dismiss();
-                        }
+                            //dismiss();
+                        //}
                         //onEditorAction(mEditText, id, new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.getMaxKeyCode()));
                         //mListener.onDialogPositiveClick(ListDialogFragment.this, mEditText.getText().toString());
                     }
